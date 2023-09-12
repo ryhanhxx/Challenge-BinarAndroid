6221566d6707e94f5a1bffe0d678ad625320e8d9
ryhanhxx <rayhan328967@gmail.com>
2023-09-12 11:42:21 +0700

    update assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,13 +30,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
-
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- app/src/main/java/com/ch/foodapp/MainActivity.kt
+++ app/src/main/java/com/ch/foodapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.ch.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ch.foodapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: Adapter
+    lateinit var list: ArrayList<Property>
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvListmakanan.setHasFixedSize(true)
+        list = ArrayList()
+        list.add(Property(R.drawable.img_ayambakar, "Ayam Bakar", "Rp. 50.000"))
+        list.add(Property(R.drawable.img_ayamgeprek, "Ayam Geprek", "Rp. 40.000"))
+        list.add(Property(R.drawable.img_ayamgoreng, "Ayam Goreng", "Rp. 40.000"))
+        list.add(Property(R.drawable.img_sateususayam, "Sate Usus Ayam", "Rp. 5.000"))
+
+        adapter = Adapter(list)
+        binding.rvListmakanan.adapter = adapter
+        binding.rvListmakanan.layoutManager = LinearLayoutManager(this)
     }
 }
