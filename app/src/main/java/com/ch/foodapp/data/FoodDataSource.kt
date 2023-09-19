2d6bcda1c54ac9919e8d425efe1f9477aac6cf2b
ryhanhxx <rayhan328967@gmail.com>
2023-09-19 19:14:15 +0700

    update assets

--- app/src/main/java/com/ch/foodapp/data/FoodDataSource.kt
+++ app/src/main/java/com/ch/foodapp/data/FoodDataSource.kt
@@ -10,32 +10,32 @@ class FoodDataSourceImpl() : FoodDataSource {
     override fun getFoodData(): List<Food> {
         return mutableListOf(
             Food(
-                imgUrl = "https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
+                imgUrl = "https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_sushi.jpg",
                 name = "Sushi",
                 price = "Rp. 28.000"
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/blob/feature_navigation_component/app/src/main/res/drawable/img_taichan.jpg",
                 name = "Taichan",
                 price = "Rp. 28.000"
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_spaghetti.jpg",
                 name = "Spaghetti",
                 price = "Rp. 28.000"
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_ayampanggang.jpg",
                 name = "Ayam Panggang",
                 price = "Rp. 28.000"
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_dimsum.jpg",
                 name = "Dim Sum",
                 price = "Rp. 28.000"
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_kentang.jpg",
                 name = "Kentang Goreng",
                 price = "Rp. 28.000"
             ),
