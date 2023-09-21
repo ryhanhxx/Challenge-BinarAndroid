e1e9235eb7657da4acfdbe07249886f6439412e8
ryhanhxx <rayhan328967@gmail.com>
2023-09-21 13:20:03 +0700

    update datasource

--- app/src/main/java/com/ch/foodapp/data/FoodDataSource.kt
+++ app/src/main/java/com/ch/foodapp/data/FoodDataSource.kt
@@ -10,14 +10,14 @@ class FoodDataSourceImpl() : FoodDataSource {
     override fun getFoodData(): List<Food> {
         return mutableListOf(
             Food(
-                imgUrl = "https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_sushi.jpg",
+                imgUrl = "https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_sushi.webp",
                 name = "Sushi",
                 price = "Rp. 40.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_taichan.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_taichan.webp",
                 name = "Taichan",
                 price = "Rp. 20.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
@@ -25,35 +25,35 @@ class FoodDataSourceImpl() : FoodDataSource {
 
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_spaghetti.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_spaghetti.webp",
                 name = "Spaghetti",
                 price = "Rp. 38.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_ayampanggang.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_ayampanggang.webp",
                 name = "Ayam Panggang",
                 price = "Rp. 18.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_dimsum.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_dimsum.webp",
                 name = "Dim Sum",
                 price = "Rp. 25.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_kentang.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_kentang.webp",
                 name = "Kentang Goreng",
                 price = "Rp. 10.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
-                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
+                imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.webp",
                 name = "Burger",
                 price = "Rp. 30.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
