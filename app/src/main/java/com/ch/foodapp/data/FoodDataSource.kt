a00ba5f8af8e1e62f4a5ad2c9a80bafa29d7f7c4
ryhanhxx <rayhan328967@gmail.com>
2023-09-21 13:13:48 +0700

    add navigation component

--- app/src/main/java/com/ch/foodapp/data/FoodDataSource.kt
+++ app/src/main/java/com/ch/foodapp/data/FoodDataSource.kt
@@ -12,14 +12,14 @@ class FoodDataSourceImpl() : FoodDataSource {
             Food(
                 imgUrl = "https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_sushi.jpg",
                 name = "Sushi",
-                price = "Rp. 28.000",
+                price = "Rp. 40.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
                 imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_taichan.jpg",
                 name = "Taichan",
-                price = "Rp. 28.000",
+                price = "Rp. 20.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
 
@@ -27,35 +27,35 @@ class FoodDataSourceImpl() : FoodDataSource {
             Food(
                 imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_spaghetti.jpg",
                 name = "Spaghetti",
-                price = "Rp. 28.000",
+                price = "Rp. 38.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
                 imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_ayampanggang.jpg",
                 name = "Ayam Panggang",
-                price = "Rp. 28.000",
+                price = "Rp. 18.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
                 imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_dimsum.jpg",
                 name = "Dim Sum",
-                price = "Rp. 28.000",
+                price = "Rp. 25.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
                 imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_kentang.jpg",
                 name = "Kentang Goreng",
-                price = "Rp. 28.000",
+                price = "Rp. 10.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             ),
             Food(
                 imgUrl="https://raw.githubusercontent.com/ryhanhxx/Challenge-BinarAndroid/feature_navigation_component/app/src/main/res/drawable/img_burger.jpg",
                 name = "Burger",
-                price = "Rp. 28.000",
+                price = "Rp. 30.000",
                 desc = "A variant of traditional Chinese dumplings served steamed and fried.",
                 quantity = 1
             )
